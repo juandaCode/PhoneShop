e94810d4ce081939b8ce4c974172f76530d55784
Daniel <juandanielzuluagaserna@gmail.com>
2021-03-07 11:48:40 -0500

    cambio de variables de entorno

--- Pedido System/config/db.php
+++ Pedido System/config/db.php
@@ -2,8 +2,8 @@
 	/*-------------------------
 	ruby benitez
 	---------------------------*/
-define("DB_HOST", "localhost");//DB_HOST:  generalmente suele ser "127.0.0.1"
-define("DB_NAME", "");//Nombre de la base de datos
-define("DB_USER", "root");//Usuario de tu base de datos
-define("DB_PASS", "");//Contraseña del usuario de la base de datos
+define("DB_HOST", "buynlkb9d4nyp0ifphv0-mysql.services.clever-cloud.com");//DB_HOST:  generalmente suele ser "127.0.0.1"
+define("DB_NAME", "buynlkb9d4nyp0ifphv0");//Nombre de la base de datos
+define("DB_USER", "ucmj2uzb2neeap58");//Usuario de tu base de datos
+define("DB_PASS", "ajnY3JrSk5veta6zt5z7");//Contraseña del usuario de la base de datos
 
